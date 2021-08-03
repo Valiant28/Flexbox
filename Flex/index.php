5d206ec723530acaa8a2df3cb95d0224450e0e57
Valiant Reza <88175677+Valiant28@users.noreply.github.com>
2021-08-03 18:22:02 +0700

    Add files via upload

---
+++ Flex/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+    <link rel="stylesheet" href="style.css">
+    <title>Flexbox</title>
+</head>
+<body>
+    
+    <div class="container">
+        <div class="layanan satu">
+            <h4>Who is Saitama?</h4>
+            <p>Saitama adalah sebuah karaktek anime yang memiliki sebuah kekuatan yang sangat tidak wajar, dikarenakan belum ada yang mampu menandingi kekuatan nya dalam serial anime One Puch Man.</p>
+        </div>
+        <div class="layanan tiga">
+            <div class="service">
+            <h4>100x</h4> Push-up
+            </div>
+            <div>
+                
+            </div>
+            <div class="service">
+            <h4>100x</h4> Sit-up
+            </div>
+            <div class="service">
+            <h4>100x</h4> Squat
+            </div>
+            <div class="service">
+            <h4>10Km</h4> Berlari
+            </div>
+            
+        </div>
+        <div class="layanan dua">
+            <div class="thubmnail">
+                <picture>
+            <img src="img/1.jpg">
+            </picture>
+            <div class="overlay"></div>
+            </div>
+        </div>
+    </div>
+            
+</body>
+</html>
